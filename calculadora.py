d242cb49c1dd09b358293fd165bc2a026f06ce5b
snozzono <164924936+snozzono@users.noreply.github.com>
2024-03-25 15:02:06 -0300

    Add files via upload

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,6 @@
 num1 = int(input("numero 1: ")) 
 num2 = int(input("numero 2: ")) 
+num3 = int(input("numero 3: "))
 
 valor = 0
 while True:
@@ -8,22 +9,26 @@ while True:
             2- Restar
             3- Multiplicar
             4- dividir 
+            5- sumar 3 valores
         """)
 
     valor = int(input("Elige una opcion: ") )     
 
     if valor == 1:
         print("la suma es",num1+num2)
-        break;
+        break
     if valor == 2:
         print("la resta es",num1-num2)
-        break;
+        break
     if valor == 3:
         print("la multiplicacion es",num1*num2)
-        break;
+        break
     if valor == 4:
         print("la division es",num1/num2)
-        break;
+        break
+    if valor == 5:
+        print("la suma es: ", num1+num2+num3)
+        break
     else:
         print("Opcion incorrecta")
-        break;
+        break
